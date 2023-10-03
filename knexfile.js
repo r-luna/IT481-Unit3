b5c22c9915c4c07db45fb2fbe6e9d40b234051ba
Ricardo Luna <rluna@lilly.com>
2023-10-03 10:44:44 -0400

    configure to connect to the database

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      database: process.env.DATABASE_NAME || 'knex-pg-db',
+      database: process.env.DATABASE_NAME || 'IT481',
       user: process.env.POSTGRES_USER || 'postgres',
-      password: process.env.POSTGRES_USER_PW || '<INSERT PASSWORD FOR postgres USER HERE>',
+      password: process.env.POSTGRES_USER_PW || 'Numbernine9',
       port: process.env.POSTGRES_PORT || 5432,
       host: process.env.POSTGRES_HOST || 'localhost'
     },
