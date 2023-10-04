82a94cb1423cf6aca909c32d619b7ae1050ab4ae
Ricardo Luna <rluna@lilly.com>
2023-10-04 00:54:49 -0400

    working shippers seed file

--- db/seeds/shippers.js
+++ db/seeds/shippers.js
@@ -6,8 +6,11 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('Shippers').del()
   await knex('Shippers').insert([
-    {'ShipperID':1,'CompanyName':'Speedy Express','Phone':'(503) 555-9831'},
-    {'ShipperID':2,'CompanyName':'United Package','Phone':'(503) 555-3199'},
-    {'ShipperID':3,'CompanyName':'Federal Shipping','Phone':'(503) 555-9931'}
+    {'ShipperID':1,'CompanyName':'Speedy Express',
+'Phone':'(503) 555-9831'},
+    {'ShipperID':2,'CompanyName':'United Package',
+'Phone':'(503) 555-3199'},
+    {'ShipperID':3,'CompanyName':'Federal Shipping',
+'Phone':'(503) 555-9931'}
   ]);
 };
