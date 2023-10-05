e3bba478cc66faf4351c0a92e300a2064abed3cd
Ricardo Luna <rluna@lilly.com>
2023-10-05 10:08:18 -0400

    add route to get total reciords of customers table

--- routes/unit2.js
+++ routes/unit2.js
@@ -2,12 +2,26 @@ const knex = require('../db/knex');
 const express = require('express');
 const router = express.Router();
 
-// GET Student data
+// GET Customer data
 router.get('/get/customers', async function(req, res, next) {
   try {
 
     await knex.select().from('Customers').select('ContactName').then((customers) =>  { 
       res.json(customers);
+    });
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// GET Customer total
+router.get('/get/customerquan', async function(req, res, next) {
+  try {
+
+    await knex.select().from('Customers').count('* as totalRows').first().then((totalObj) =>  { 
+      res.json(totalObj);
     });
 
   } catch (err) {
