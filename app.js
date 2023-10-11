ac6a1dfb90d3483ec14ad68e770338a4d4990baa
Ricardo Luna <rluna@lilly.com>
2023-10-10 21:28:31 -0400

    refactor routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var apiRouter = require('./routes/api/v1');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
