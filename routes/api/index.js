743d4c35a40e1bac2ec60d9b85ba146ce7e8b151
Ricardo Luna <rluna@lilly.com>
2023-10-10 22:48:24 -0400

    completed login route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-//API version 1
-
 router.use('/v1/customer', require('./v1/customer/unit2'));
 router.use('/v1/user/create', require('./v1/user/create.js'));
-router.use('/v1/user/create', require('./v1/user/login.js'));
+router.use('/v1/user/login', require('./v1/user/login.js'));
+//router.use('/v1/user/authenticate', require('./v1/user/authenticate.js'));
 
 module.exports = router;
--- routes/api/v1/user/login.js
+++ routes/api/v1/user/login.js
@@ -5,29 +5,33 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 router.post('/', ( request, response ) => {
-  const { user } = request.body
-  database("users")
-    .where({username: user.username })
-    .first()
-    .then(retrievedUser => {
-        if(!retrievedUser) throw new Error("user not found!")
-        return Promise.all([
-            bcrypt.compare(user.password, retrievedUser.password_hash),
-            Promise.resolve(retrievedUser)
-        ]).then(results => {
-            const areSamePasswords = results[0]
-            if(!areSamePasswords) throw new Error("wrong Password!")
-            const user = results[1]
-            const payload = {username: user.username}
-            const secret =  "SECRET"
-            jwt.sign(payload, secret, (error, token) => {
-                if(error) throw new Error("Sign in error!")
-                response.json({token, user})
-            }).catch(error => {
-                response.json({message: error.message})
-            })
-        })
-    })
+  const { user } = request.body;
+  database('users')
+      .where({username: user.username })
+      .first()
+      .then(retrievedUser => {
+          if(!retrievedUser) throw new Error('user not found!')
+          return Promise.all([
+              bcrypt.compare(user.password, retrievedUser.password_hash),
+              Promise.resolve(retrievedUser)
+          ]).then(results => {
+              const areSamePasswords = results[0]
+              if (!areSamePasswords) {
+                throw new Error('wrong Password!');
+              }
+              const user = results[1];
+              const payload = {username: user.username};
+              const secret =  'SECRET';
+              jwt.sign(payload, secret, (error, token) => {
+                  if (error !== null) {
+                    throw new Error('Sign in error!');
+                  }
+                  response.json({token, user});
+              });
+          })
+      }).catch(error => {
+        response.status(401).json({message: error.message});
+      });
 });
 
 module.exports = router;
