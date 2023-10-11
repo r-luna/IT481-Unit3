c873890176d78e51bf325978fc1460bf887e2b70
Ricardo Luna <rluna@lilly.com>
2023-10-11 09:52:34 -0400

    add role column to user table

--- db/migrations/20231009145652_addUserTable.js
+++ db/migrations/20231009145652_addUserTable.js
@@ -1,20 +1,21 @@
 /**
- * @param { import("knex").Knex } knex
+ * @param { import('knex').Knex } knex
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-  await knex.schema.createTable("users", (table)=> {
-    table.increments("id");
-    table.string("username");
-    table.string("password_hash")
+  await knex.schema.createTable('users', (table)=> {
+    table.increments('id');
+    table.string('username');
+    table.string('password_hash');
+    table.string('role');
     }
 )
 };
 
 /**
- * @param { import("knex").Knex } knex
+ * @param { import('knex').Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = async function(knex) {
-  await knex.schema.dropTableIfExists("users");
+  await knex.schema.dropTableIfExists('users');
 };
