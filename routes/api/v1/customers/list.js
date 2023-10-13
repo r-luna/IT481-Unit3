4803a53311310b378a9a2f2c377e02ad07e40027
Ricardo Luna <rluna@lilly.com>
2023-10-12 23:06:53 -0400

    route that returns all customers

---
+++ routes/api/v1/customers/list.js
@@ -0,0 +1,23 @@
+const knex = require('../../../../db/knex');
+const express = require('express');
+const router = express.Router();
+const authenticate = require('../../../../middleware/authenticate.js');
+
+// GET Customer data
+router.get('/', authenticate, async function(req, res, next) {
+  try {
+    if (req.role !== 'SalesRole' && req.role !== 'CEORole') {
+      res.status(401).send('Unauthorized');
+    } else {
+      await knex.select('*').from('Customers').then((orders) =>  { 
+        res.json(orders);
+      });
+    }
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+});
+
+
+module.exports = router;
