2c91900e0788d8b85a9abe73308c09146a6568cc
Ricardo Luna <rluna@lilly.com>
2023-10-12 23:08:16 -0400

    updated to reflect new routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/v1/customer', require('./v1/customer/unit2'));
+router.use('/v1/orders/list', require('./v1/orders/list.js'));
+router.use('/v1/customers/list', require('./v1/customers/list.js'));
 router.use('/v1/user/create', require('./v1/user/create.js'));
 router.use('/v1/user/login', require('./v1/user/login.js'));
-//router.use('/v1/user/authenticate', require('./v1/user/authenticate.js'));
+
 
 module.exports = router;
