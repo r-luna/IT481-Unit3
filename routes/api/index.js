4b7d32f99186d3bd7f54b45bf1c7603ac98dd712
Ricardo Luna <rluna@lilly.com>
2023-10-12 23:23:32 -0400

    added get all employees route

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.use('/v1/customer', require('./v1/customer/unit2'));
 router.use('/v1/orders/list', require('./v1/orders/list.js'));
 router.use('/v1/customers/list', require('./v1/customers/list.js'));
+router.use('/v1/employees/list', require('./v1/employees/list.js'));
 router.use('/v1/user/create', require('./v1/user/create.js'));
 router.use('/v1/user/login', require('./v1/user/login.js'));
 
